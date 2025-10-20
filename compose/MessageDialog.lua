dfb039f36ec7e4f72aff89cca9a1b1829166fc69
hexaredecimal <mfanakagama@gmail.com>
2025-10-20 19:45:06 +0200

    MessageDialog: This is now a rendeble component

--- compose/MessageDialog.lua
+++ compose/MessageDialog.lua
@@ -5,7 +5,7 @@ end
 
 local Slab = require (SLAB_PATH .. 'API')
 local UI = require(SLAB_PATH .. "UI")
-local State = require(SLAB_PATH .. "State")
+local Remember = require(SLAB_PATH .. "Remember")
 
 local MessageDialog = {}
 MessageDialog.__index = MessageDialog
@@ -14,28 +14,25 @@ MessageDialog.__index = MessageDialog
 setmetatable(MessageDialog, {
     __call = function(_, opts)
         local self = setmetatable({}, MessageDialog)
-        self.title = opts.title or ""
+        self.title = opts.title or "A Message Dialog"
         self.title = tostring(self.title)
         
         self.text = opts.text or ""
         self.text = tostring(self.text)
-        local show, setShow = State.of(self.title, true)
-        self.onClose = opts.onClose or function() end
-        
-        if show then
-          local Result = Slab.MessageBox(self.title, self.text)
-
-          if Result ~= "" then
-            setShow(false)
-            self.onClose()
-          end
-        end
-        
+        self.id = "MessageDialog_" .. UI.nextId()
+        self.show = opts.show or false
+        self.onClose = opts.onClose or function() end        
         return self
     end
 })
 
 function MessageDialog:render()
+    if self.show then
+      local Result = Slab.MessageBox(self.title, self.text)
+      if Result ~= "" then
+        self.onClose(false)
+      end
+    end
 end
 
 
